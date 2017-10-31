42d5b8451888644bd5fd5ed6f2a838bbbb038f76
Your Name <494738945@qq.com>
2017-10-31 20:47:25 +0800

    页面基本架子完成

--- public/myself/js/common.js
+++ public/myself/js/common.js
@@ -3,8 +3,8 @@ if (location.href.indexOf("login.html") < 0 ){
         url: "/employee/checkRootLogin",
         type:"get",
         success:function (data) {
-            if (data.error == 400){
-                location.href = "index.html";
+            if (data.error ===400){
+                location.href = "login.html";
             }
         }
     });
@@ -19,4 +19,34 @@ $(document).ajaxStop(function () {
     setTimeout(function () {
         NProgress.done();
     },500)
+});
+
+
+$(function () {
+    $(".child").prev().on("click",function () {
+        $(this).next().slideToggle();
+    })
+
+
+
+    $(".icon_menu").on("click", function () {
+        //让侧边栏慢慢的出去
+        $(".aside").toggleClass("now");
+        //慢慢的变长
+        $(".main").toggleClass("now");
+    });
+    $(".icon_logout").on("click",function () {
+        $("#logoutModal").modal("show");
+    })
+    $(".btn_logout").on("click",function () {
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function (data) {
+                if (data.success){
+                    window.location.href = "login.html";
+                }
+            }
+        })
+    })
 });
