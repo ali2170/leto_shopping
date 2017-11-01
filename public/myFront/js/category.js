56f33a95250b3909cd59d78835a70bf92082f671
Your Name <494738945@qq.com>
2017-11-02 02:54:34 +0800

    前端分类页面完成

--- public/myFront/js/category.js
+++ public/myFront/js/category.js
@@ -1,3 +1,42 @@
 /**
  * Created by TT-LOVE on 2017/11/1.
  */
+mui('.left .mui-scroll-wrapper').scroll({
+    indicators:false
+});
+
+var scroll = mui('.right .mui-scroll-wrapper').scroll({
+    indicators:false
+});
+
+$.ajax({
+    type:"get",
+    url:"/category/queryTopCategory",
+    // data:,
+    success:function (data) {
+        var html = template("tpl",data);
+        $(".left ul").html(html);
+        renderSecond(data.rows[0].id);
+    }
+});
+$(".left").on("click","li",function () {
+    $(this).addClass('now').siblings().removeClass('now');
+    var id = $(this).data("id");
+    renderSecond(id);
+    scroll.scrollTo(0,0,100);//100毫秒滚动到顶
+})
+function renderSecond(id){
+    $.ajax({
+        type:'get',
+        url:"/category/querySecondCategory",
+        data:{
+            id:id
+        },
+        success:function (data) {
+            var html = template("tpl2",data);
+            console.log(data);
+            $(".right ul").html(html);
+        }
+    })
+}
+
